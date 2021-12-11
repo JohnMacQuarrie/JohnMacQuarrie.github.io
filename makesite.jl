13524b0eca3d90aa796ac1601bbd65ddfd164595
JohnMacQuarrie <92113476+JohnMacQuarrie@users.noreply.github.com>
2021-12-11 20:43:21 -0300

    .

--- makesite.jl
+++ makesite.jl
@@ -1,24 +1,32 @@
 # the idea is that this program will modify html documents that can be pushed to the site.  I'll make a text document 
 
 
-oldpage = read("./contact/index.html", String)
+pages = [
+    "./index.html", 
+    "./contact/index.html"
+]
 
-menu = read("./englishheader.html", String)
+for currentpage in pages
+    
+    oldpage = read(currentpage, String)
 
+    r = nothing
 
-r = findnext("<!--ENGPREAMBLE-->", oldpage, 1) 
+    r = findnext("<!--ENGPREAMBLE-->", oldpage, 1) 
 
-if typeof(r) != Nothing
-    s = last(r)
-    t = first( findnext("<!--ENGPREAMBLE-->", oldpage, s) )
-  #  series = series[1:s]*"{"*series[s+2:end]
-  #   series = series[1:t-1]*"}"*series[t+1:end]
- end
+    if typeof(r) != Nothing
+        s = last(r)
+        t = first( findnext("<!--ENGPREAMBLE-->", oldpage, s) )
 
-newpage = oldpage[1:s]*menu*oldpage[t:end]
+        menu = read("./englishheader.html", String)
 
-page = open("./contact/index.html", "w")
+        newpage = oldpage[1:s]*menu*oldpage[t:end]
 
-write(page, newpage)
+    else
+        newpage = oldpage
+    end
 
-close(page)
+    page = open(currentpage, "w")
+    write(page, newpage)
+    close(page)
+end
